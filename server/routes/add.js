fbc434b1d9e7a4d8350ad04bcf50f5bf43772caa
Ehud Adler <easports96@gmail.com>
2020-03-24 00:40:37 -0400

    fixed stuff

--- server/routes/add.js
+++ server/routes/add.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', function(req, res, next) {
-    console.log(req.body.title);
-    var query = `insert into shiurim (title, link, sources, date, lecturer, institution) VALUES ('${req.body.title}', '${req.body.link}', '${req.body.sources}', '${req.body.date}', '${req.body.lecturer}', '${req.body.institution}')`
-    res.locals.connection.query(query, function (error, results, fields) {
-        if(error) throw error;
-        res.send(JSON.stringify(results));
-    });
+    if(req.body.title === undefined || req.body.link === undefined  || req.body.date === undefined || req.body.lecturer === undefined || req.body.institution === undefined) {
+        res.status(400).send('Bad Request');
+    } else {
+        var query = sanitizer.escape()`insert into shiurim (title, link, sources, date, lecturer, institution) VALUES ('${req.sanitize(req.body.title)}', '${req.sanitize(req.body.link)}', '${req.sanitize(req.body.sources)}', '${req.sanitize(req.body.date)}', '${req.sanitize(req.body.lecturer)}', '${req.sanitize(req.body.institution)}')`
+        res.locals.connection.query(query, function (error, results, fields) {
+            if(error) res.status(400).send('Bad Request');
+            res.send(JSON.stringify(results));
+        });
+    }
 });
 
 module.exports = router; 
--- server/routes/shiurim.js
+++ server/routes/shiurim.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.locals.connection.query(`select * from shiurim where date >= NOW() ORDER BY date DESC `, function (error, results, fields) {
-        if(error) throw error;
-        res.send(JSON.stringify(results));
+        if(error) res.status(400).send('Bad Request');
+        else res.send(JSON.stringify(results));
     });
 });
 
